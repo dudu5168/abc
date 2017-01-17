dc445b6cbd87a9290ec6f12d2836a7d6fa25a632
dudu5168 <757103766@qq.com>
2017-01-17 11:37:07 +0800

    update

---
+++ js/myplugin.js
@@ -0,0 +1,14 @@
+/**
+ * myPlugin
+ */
+$.fn.myPlugin = function(options) {
+  var defaults = {
+    'color': 'red',
+    'fontSize': '14px'
+  };
+  var settings = $.extend(defaults, options);
+  return this.css({
+    'color': settings.color,
+    'fontSize': settings.fontSize
+  });
+}
